31bd4ed555700bfa8c10d349cdd54b22ae7eaa07
Catherine <yacatherine19@gmail.com>
2017-07-21 13:02:24 +0800

    added OkHttp

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # WebServices
+
+## Connect to network
+1. Upload data
+2. Download data
+3. Display online content
+![1][1]
+
+
+
+
+
+
+
+[Tutorial]:<http://www.runoob.com/w3cnote/android-tutorial-http.html>
+
+[1]: https://github.com/Catherine22/Algorithms/blob/master/res/tree.png
--- app/src/main/java/com/catherine/webservices/MainActivity.java
+++ app/src/main/java/com/catherine/webservices/MainActivity.java
@@ -3,8 +3,15 @@ package com.catherine.webservices;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.catherine.webservices.tasks.SampleAsyncTask;
 import com.catherine.webservices.toolkits.CLog;
 import com.catherine.webservices.toolkits.Utils;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends Activity {
     private final static String TAG = "MainActivity";
@@ -17,5 +24,8 @@ public class MainActivity extends Activity {
         CLog.d(TAG, "isNetworkHealth:" + Utils.isNetworkHealth(MainActivity.this));
         CLog.d(TAG, "isWifi:" + Utils.isWifi(MainActivity.this));
         Utils.listenToNetworkState(MainActivity.this);
+        new SampleAsyncTask().execute("param1");
     }
+
+
 }
