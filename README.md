0ca8443baa52ffc52a38a302724567ffe076e7b2
Catherine <yacatherine19@gmail.com>
2017-07-24 16:19:29 +0800

    added xml module, junit sample

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # WebServices
 
+### XML
+保存数据和彼此关系的一种格式。
 
 [Tutorial]:<http://www.runoob.com/w3cnote/android-tutorial-http.html>
---
+++ app/src/test/java/com/catherine/webservices/UnitTestSample2.java
@@ -0,0 +1,50 @@
+package com.catherine.webservices;
+
+import android.content.Context;
+
+import com.catherine.webservices.toolkits.Utils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Catherine on 2017/7/24.
+ * Soft-World Inc.
+ * catherine919@soft-world.com.tw
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class UnitTestSample2 {
+
+    private static final String FAKE_STRING = "MB890CPC";
+
+    @Mock
+    Context mMockContext;
+
+    @Test
+    public void readStringFromMethod_LocalizedString() {
+        Utils utils = new Utils();
+
+        // ...when the string is returned from the object under test...
+        String result = utils.junitTestSample2();
+
+        // ...then the result should be the expected one.
+        assertThat(result, is(FAKE_STRING));
+    }
+
+    @Test
+    public void readStringFromContext_LocalizedString() {
+        Utils utils = new Utils();
+
+        // ...when the string is returned from the object under test...
+        String result = utils.JUNIT_TEST;
+
+        // ...then the result should be the expected one.
+        assertThat(result, is(FAKE_STRING));
+    }
+}
