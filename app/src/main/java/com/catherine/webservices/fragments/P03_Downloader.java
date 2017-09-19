6bfabc8c52c95f1e850d08c0269b3948a2edd24b
Catherine <yacatherine19@gmail.com>
2017-09-19 15:05:35 +0800

    fixed bugs

--- app/src/main/java/com/catherine/webservices/fragments/P03_Downloader.java
+++ app/src/main/java/com/catherine/webservices/fragments/P03_Downloader.java
@@ -247,8 +247,10 @@ public class P03_Downloader extends LazyFragment {
             @Override
             public void onRefresh() {
                 for (int i = 0; i < tasks.length; i++) {
-                    tasks[i].stop();
-                    tasks[i].cancel(true);
+                    if (tasks[i] != null) {
+                        tasks[i].stop();
+                        tasks[i].cancel(true);
+                    }
                     updateView(i, IDLE);
                 }
                 init();
